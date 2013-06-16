da67e847834e5b832f7ca1a5c4b3d7a5b5db6ed7
ilayze <ilayndeganit@gmail.com>
2013-06-16 16:57:58 +0300

    Added view model that passes as parameter to delete a main category

---
+++ MvcDeganitSite/ViewModels/DeleteMainCategory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MvcDeganitSite.Models;
+
+namespace MvcDeganitSite.ViewModels
+{
+    //used in MainCategoryController/Delete(int id)
+    public class DeleteMainCategory
+    {
+        public MainCategory mainCategory { get; set; }
+        public IEnumerable<Recipe> recipes { get; set; }
+    }
+}
