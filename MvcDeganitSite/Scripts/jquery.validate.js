1c357f85ecc09e8e0f5f7a30f855ce1deeeafbc7
ilayze <ilayndeganit@gmail.com>
2013-06-20 21:20:29 +0300

    change error message to hebrew

--- MvcDeganitSite/Scripts/jquery.validate.js
+++ MvcDeganitSite/Scripts/jquery.validate.js
@@ -255,7 +255,7 @@ $.extend($.validator, {
 	},
 
 	messages: {
-		required: "This field is required.",
+		required: "שדה חובה",
 		remote: "Please fix this field.",
 		email: "Please enter a valid email address.",
 		url: "Please enter a valid URL.",
