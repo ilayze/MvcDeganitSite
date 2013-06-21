d3b36c982e0b72202437188ad9a77ed1e5491f0e
ilayze <ilayndeganit@gmail.com>
2013-06-21 14:06:32 +0300

    Added filter by user and refactoring of function patameters names

--- MvcDeganitSite/Controllers/HomeController.cs
+++ MvcDeganitSite/Controllers/HomeController.cs
@@ -13,28 +13,44 @@ namespace MvcDeganitSite.Controllers
     public class HomeController : Controller
     {
         private RecipesContext db = new RecipesContext();
-        public ActionResult Index(int? id,string name)
+        public ActionResult Index(int? categoryId,string navigationName)
         {
            string username= User.Identity.Name;
            if (String.IsNullOrEmpty(username))
            {
                return RedirectToAction("LogOn","Account");
            }
+
+           if (username == "דגנית")
+           {
+               ViewBag.Administrator = true;
+           }
+           else
+           {
+               ViewBag.Administrator = false;
+           }
+
             var viewModel = new CategoryToDisplay();
             viewModel.MainCategories = db.MainCategories;
             viewModel.NavigateList = db.NavigationWords;
            
             ViewBag.Message = "אתר המתכונים של דגנית";
-            if (id != null)
+            if (categoryId != null)
             {
-                viewModel.mainCategoryId = viewModel.MainCategories.Where(m => m.MainCategoryId == id).Single().MainCategoryId;
+                viewModel.mainCategoryId = viewModel.MainCategories.Where(m => m.MainCategoryId == categoryId).Single().MainCategoryId;
                 viewModel.Recipes = db.Recipes.Where(r => r.MainCategoryID == viewModel.mainCategoryId);
-                ViewBag.Name = viewModel.MainCategories.Where(m => m.MainCategoryId == id).Single().Name;
+                //filters by current user
+                viewModel.Recipes = from recipe in viewModel.Recipes
+                                    from user in recipe.Users
+                                    where user.Name == username
+                                    select recipe;
+                                  
+                ViewBag.Name = viewModel.MainCategories.Where(m => m.MainCategoryId == categoryId).Single().Name;
             }
             
-            if (!String.IsNullOrEmpty(name))
+            if (!String.IsNullOrEmpty(navigationName))
             {
-                var navWord = db.NavigationWords.Where(n => n.Name == name).Single();
+                var navWord = db.NavigationWords.Where(n => n.Name == navigationName).Single();
                 viewModel.chosenNavigationWord = navWord;
 
                 List<Recipe> recipesByNavigationWords = new List<Recipe>();
@@ -49,11 +65,16 @@ namespace MvcDeganitSite.Controllers
                     }
                 }
 
+                recipesByNavigationWords = (from recipe in recipesByNavigationWords
+                                           from user in recipe.Users
+                                           where user.Name == username
+                                           select recipe).ToList();
+
                 viewModel.RecipeByChoosenNavigateWord = recipesByNavigationWords;
                     
 
                // viewModel.RecipeByChoosenNavigateWord = db.Recipes.Where(n =>).
-                ViewBag.Nav = name;
+                ViewBag.Nav = navigationName;
             }
             
             
